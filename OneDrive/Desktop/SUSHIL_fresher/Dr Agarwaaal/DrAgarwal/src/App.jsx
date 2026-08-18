6b012351e8d2a56b84927f2222120a878b7cd868
RAJESH KUMAR <sushilchandran9927@gmail.com>
2026-08-18 18:07:06 +0530

    InitialStage

---
+++ OneDrive/Desktop/SUSHIL_fresher/Dr Agarwaaal/DrAgarwal/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './CSS/Header.css'
+import Header from './components/Header.jsx'
+
+function App() {
+  
+
+  return (
+    <div className="OverallView">
+    <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ OneDrive/Desktop/SUSHIL_fresher/Dr Agarwaaal/DrAgarwal/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import  react from 'react'
+import '../CSS/Header.css'
+
+const Header  =  ()=>{
+
+return(
+	<div>Hsss</div>
+	)
+}
+
+
+
+export default Header;
