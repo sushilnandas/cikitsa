d3dfffb4c4c5ae2d93c262e58f49da9fa7563c39
sushilnandas <sushilchandran9927@gmail.com>
2026-08-21 19:05:47 +0530

    neurology

--- src/components/CenterMain.jsx
+++ src/components/CenterMain.jsx
@@ -4,6 +4,8 @@ const CenterMain = () =>{
 
 	return (
 	<div className='MiddlePhase' style={{border: "1px solid red"}}>
+
+		<img src="https://wallpaperbat.com/img/1957366-online-certification-program-in-chiropractic-neurology-ccn.jpg" alt="" />
 HEAD
 </div>
 	
