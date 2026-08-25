883f9a8feb6eb269f687b9dc67b139840db909b5
RAJESH KUMAR <sushilchandran9927@gmail.com>
2026-08-25 18:35:11 +0530

    MainPage

---
+++ OneDrive/Desktop/SUSHIL_fresher/WebPages/Page4/DrAgarwalUpdated/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './CSS/Header.css'
+import Header from './components/Header.jsx'
+import CenterMain from './components/CenterMain.jsx'
+
+function App() {
+  
+
+  return (
+    <div className="OverallView">
+        <img src="https://wallpaperbat.com/img/1957366-online-certification-program-in-chiropractic-neurology-ccn.jpg" alt="" />
+
+    <Header/>
+    <CenterMain/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ OneDrive/Desktop/SUSHIL_fresher/WebPages/Page4/DrAgarwalUpdated/src/components/CenterMain.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../CSS/CenterMain.css'
+const CenterMain = () =>{
+
+	return (
+	<div className='MiddlePhase' style={{border: "1px solid red"}}>
+
+	<p>
+     A paragraph is a group of sentences related to one topic or developing one point.
+These sentences are closely related in meaning and are arranged in a systematic manner.
+A paragraph may stand independently, that is, it may be a short composition complete in itself.
+It may also be a part of a longer composition like an essay.
+The essential features of a good paragraph are: unity, coherence, emphasis and variety.
+	</p>
+
+
+
+
+	<div>
+               
+		</div>
+
+
+</div>
+	
+		)
+}
+export default CenterMain
+
+
+
+
